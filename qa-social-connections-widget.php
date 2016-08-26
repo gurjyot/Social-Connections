9e84fea73af78c1e9bf9be6b39c66c3c6858383e
Gurjyot <gurjyottheman@gmail.com>
2016-08-27 02:53:49 +0530

    Making Buttons Responsive

--- qa-social-connections-widget.php
+++ qa-social-connections-widget.php
@@ -9,18 +9,26 @@ class qa_social_connections_widget {
         return true;
     }
 
-function allow_region($region)
-{
-    return true;
-}
+    function allow_region($region)
+    {
+        return true;
+    }
 
-function output_widget($region, $place, $themeobject, $template, $request, $qa_content)
-{
-    echo 	'<div class= "sc1"><div class= "sc2"><span>Connect with us:</span>
-    		<div class= "sc3"><a href="http://www.facebook.com"><div class="facebook-icon"></div></a>
-			<a href="http://www.twitter.com"><div class="twitter-icon"></div></a>
-			<a href="http://www.google.com"><div class="google-icon"></div></a>
-			<a href="http://www.instagram.com"><div class="instagram-icon"></div></a></div></div>';
-}
+    function output_widget($region, $place, $themeobject, $template, $request, $qa_content)
+    {
+        echo 	'<div class= "sc1">
+                    <div class= "sc2">
+                        <div class= "sc3">
+                            <span>Connect with us:</span>
+                        </div>
+        	                <div class= "sc4">
+                                 <a href="http://www.facebook.com" title="Like us on Facebook"><div class="facebook-icon"></div></a>
+    			                 <a href="http://www.twitter.com" title="Follow us on Twitter"><div class="twitter-icon"></div></a>
+    			                 <a href="http://www.google.com" title="Like us on Google+"><div class="google-icon"></div></a>
+    			                 <a href="http://www.instagram.com" title="Follow us on Instagram"><div class="instagram-icon"></div></a>
+                            </div>
+                    </div>
+                </div>';
+    }
 }
 
